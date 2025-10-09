215fa18ed626d827751603c70c1ccd953aa3879c
WadkarNaved15 <navedwadkar9@gmail.com>
2025-10-09 21:54:24 +0530

    Updated Next config

--- next.config.js
+++ next.config.js
@@ -12,6 +12,22 @@ const nextConfig = {
       },
     ],
   },
+  async headers() {
+    return [
+      {
+        source: '/sitemap.xml',
+        headers: [
+          { key: 'Content-Type', value: 'application/xml' },
+        ],
+      },
+      {
+        source: '/sitemap-0.xml',
+        headers: [
+          { key: 'Content-Type', value: 'application/xml' },
+        ],
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
