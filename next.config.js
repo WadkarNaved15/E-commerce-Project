bc7dc2297029832668815dfe092564270c93918c
WadkarNaved15 <navedwadkar9@gmail.com>
2025-10-09 21:59:36 +0530

    Updated the next config

--- next.config.js
+++ next.config.js
@@ -14,19 +14,21 @@ const nextConfig = {
   },
   async headers() {
     return [
-      {
-        source: '/sitemap.xml',
-        headers: [
-          { key: 'Content-Type', value: 'application/xml' },
-        ],
-      },
-      {
-        source: '/sitemap-0.xml',
-        headers: [
-          { key: 'Content-Type', value: 'application/xml' },
-        ],
-      },
-    ];
+    {
+      source: '/sitemap.xml',
+      headers: [
+        { key: 'Content-Type', value: 'application/xml' },
+        { key: 'Cache-Control', value: 'public, max-age=0, must-revalidate' },
+      ],
+    },
+    {
+      source: '/sitemap-0.xml',
+      headers: [
+        { key: 'Content-Type', value: 'application/xml' },
+        { key: 'Cache-Control', value: 'public, max-age=0, must-revalidate' },
+      ],
+    },
+  ];
   },
 };
 
